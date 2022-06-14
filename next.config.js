1fcda856ceefa2199b57573eeb00217c15c07de9
napthedev <phongna.dev@gmail.com>
2022-06-15 00:09:28 +0700

    Refactor: reduce bundle size using preact and null-loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat",
+    };
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\b(?:graphql|graphql-request)\b/gi,
+        use: "null-loader",
+      });
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
